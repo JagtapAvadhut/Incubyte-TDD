de1d3aee0f28176d5a5541af7005c4dc4e6358a4
JagtapAvadhut <avadhutjagtap9999@gmail.com>
2024-10-26 14:11:02 +0530

    Working code for adding one number

---
+++ src/main/java/com/incubyte/dto/StringCalculator.java
@@ -0,0 +1,11 @@
+package com.incubyte.dto;
+
+public class StringCalculator {
+
+    public static int add(String input){
+        if(input.isEmpty()) {
+            return 0;
+        }
+        return Integer.parseInt(input);
+    }
+}
