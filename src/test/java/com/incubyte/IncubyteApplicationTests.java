5a0b329c9ce2b456a87382f019232b43ffff5625
JagtapAvadhut <avadhutjagtap9999@gmail.com>
2024-10-26 14:16:12 +0530

    Failed test for new line

--- src/test/java/com/incubyte/IncubyteApplicationTests.java
+++ src/test/java/com/incubyte/IncubyteApplicationTests.java
@@ -33,4 +33,9 @@ class IncubyteApplicationTests {
 		assertEquals(6, StringCalculator.add("1,2,3"));
 	}
 
+	@Test
+	public void testNewLine() {
+		assertEquals(6, StringCalculator.add("1\n2,3"));
+	}
+
 }
