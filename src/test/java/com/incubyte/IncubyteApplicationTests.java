922b7790cb4eac1261cdbeff8f86da77578ffe18
JagtapAvadhut <avadhutjagtap9999@gmail.com>
2024-10-26 14:06:11 +0530

    Failed test for empty string

--- src/test/java/com/incubyte/IncubyteApplicationTests.java
+++ src/test/java/com/incubyte/IncubyteApplicationTests.java
@@ -3,11 +3,18 @@ package com.incubyte;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class IncubyteApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testEmptyString() {
+		assertEquals(0, StringCalculator.add(""));
 	}
 
 }
